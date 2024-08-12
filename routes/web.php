16aca26a823e57606e9cdb3303115f432e115781
Matthew Fekumoh <mmattez2009@gmail.com>
2024-08-13 00:28:48 +0100

    updated the route

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 
 Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
 Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
-Route::get('/roles/{id}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+Route::get('/roles/{id}', [RoleController::class, 'edit'])->name('roles.edit');
 Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
 Route::put('/roles/{id}', [RoleController::class, 'update'])->name('roles.update');
 Route::delete('/roles/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
